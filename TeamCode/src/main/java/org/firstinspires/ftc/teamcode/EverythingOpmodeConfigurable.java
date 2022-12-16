f08ff442a4433454a5b58cd83e482895b70b21a6
MediumAutomaton <michael.ryan.worden@gmail.com>
2022-12-15 19:22:59 -0600

    Work on elevator code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/EverythingOpmodeConfigurable.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/EverythingOpmodeConfigurable.java
@@ -7,10 +7,14 @@ package org.firstinspires.ftc.teamcode;
 
 //Bugs:
 //RESOLVED - Motors keep running when sticks return to 0
-//drivePowerDown not working
+//RESOLVED (accidentally used less than instead of greater than - < instead of > - for debounce timer) drivePowerDown not working
 //Elevator up/down analog working weird - TODO: needs to switch runmode!
 
-import android.app.job.JobInfo;
+//Additional Notes:
+/*
+* - UpperLimit and LowerLimit are gone, since they have not been implemented in hardware.
+*   - TODO: Replace UpperLimit and LowerLimit with encoders (limit value vars now exist!)
+* */
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
@@ -25,7 +29,9 @@ import com.qualcomm.robotcore.util.Range;
 //Start+A for driving, Start+B for manipulator
 public class EverythingOpmodeConfigurable extends LinearOpMode {
 
+    //------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
     //Declaring variables for controls
+    //Drive (forward/backward)
     private double driveAxis;
     private boolean forwardFull;
     private static final double forwardFullPower = 1.0;
@@ -39,6 +45,8 @@ public class EverythingOpmodeConfigurable extends LinearOpMode {
     private static final double reverseSlowPower = -0.25;
     private boolean drivePowerUp;
     private boolean drivePowerDown;
+
+    //Turn
     private double turnAxis;
     private boolean turnLeftFull;
     private static final double turnLeftFullPower = -1.0;
@@ -46,6 +54,8 @@ public class EverythingOpmodeConfigurable extends LinearOpMode {
     private static final double turnRightFullPower = 1.0;
     private double turnLeftAxis;
     private double turnRightAxis;
+
+    //Strafe
     private double strafeAxis;
     private boolean strafeLeftFull;
     private static final double strafeLeftFullPower = -1.0;
@@ -53,10 +63,21 @@ public class EverythingOpmodeConfigurable extends LinearOpMode {
     private static final double strafeRightFullPower = 1.0;
     private double strafeLeftAxis;
     private double strafeRightAxis;
+
+    //Manipulator - TODO: Needs radical expansion
     private boolean clawToggle;
     private boolean armToggle;
+    //Values
+    private static final double clawClosedPosition = 0.26;
+    private static final double clawOpenPosition = 0;
+    private static final double armLeftPosition = 0.1;// TODO: Left/right are definitely swapped (New: Left=Right - they are wrong)
+    private static final double armRightPosition = 0.6;
     private static final int manipulatorToggleTimer = 500;
+
+    //Elevator
     private double elevatorAxis;
+    private static final double elevatorAxisUpperLimit = 1.0;
+    private static final double elevatorAxisLowerLimit = 0.5;//Elevator down is assisted by gravity; use lower power.
     private boolean elevatorUpFull;
     private static final double elevatorUpFullPower = 0.3;
     private boolean elevatorDownFull;
@@ -72,29 +93,27 @@ public class EverythingOpmodeConfigurable extends LinearOpMode {
     private static final int elevatorTierToggleTimer = 1000;
     private boolean elevatorEncoderReset;
     private static final double elevatorHoldPower = 0.2;
+    //Upper/Lower Limits - tiers are currently derived from these (tier 0 is lower, tier 3 is upper, tiers 1 and 2 are right in between)
+    private static final double elevatorPositionUpperLimit = 850;
+    private static final double elevatorPositionLowerLimit = 150;
 
-    //Claw and arm positions
-    private static final double clawClosedPosition = 0.26;
-    private static final double clawOpenPosition = 0;
-    private static final double armLeftPosition = 0;// TODO: Left/right could be wrong, untested
-    private static final double armRightPosition = 0.6;
+    //-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
-    //Defining variables for everything else
+    //Declaring variables or creating objects for everything else
     private DcMotor elevatorDrive;
     private int elevatorTier;
-    private Servo servo1;//Claw Servo
-    private Servo servo2;//Arm Servo
+    private Servo clawServo;
+    private Servo armServo;
     //wheel motors
     private DcMotor leftFront = null;
     private DcMotor rightFront = null;
     private DcMotor leftBack = null;
     private DcMotor rightBack = null;
 
-    private DigitalChannel upperLimit;
-    private DigitalChannel lowerLimit;
     private double elevatorMaxPower = 0.5;//Change this to adjust the max motor power, or use the D-Pad to adjust it at runtime.
     private double maxDrivePower = 0.5;
 
+    //Declaring or initializing various variables used later
     private String status;
 //    private int tier = 0;
     private double dtemp = 0;
@@ -103,6 +122,8 @@ public class EverythingOpmodeConfigurable extends LinearOpMode {
     private double elevatorPower = 0;
     private double strafePower;
     private double turnPower;
+    private double upPower;
+    private double downPower;
 
     private void setControls() {
         //   ||| CURRENTLY configured to this layout: https://wordenhome.neocities.org/rd/lm/cl125.html |||
@@ -118,7 +139,8 @@ public class EverythingOpmodeConfigurable extends LinearOpMode {
         //Slow drive is an all-or-nothing deal, so these must be booleans (on/off).
         forwardSlow = gamepad1.right_bumper;//boolean
         reverseSlow = gamepad1.left_bumper;//boolean
-        //New! Like elevator motor power, change drive motor power!
+
+        //Drive Miscellany
         drivePowerUp = gamepad1.dpad_up;//boolean
         drivePowerDown = gamepad1.dpad_down;//boolean
 
@@ -138,8 +160,8 @@ public class EverythingOpmodeConfigurable extends LinearOpMode {
 
         //Manipulator
         clawToggle = gamepad2.a;//boolean
-        armToggle = gamepad2.x;//boolean //NOTE: Could become a double in the future, for analog
-        //arm position rather than an either-or situation
+        armToggle = gamepad2.x;//boolean //TODO: Absolutely MUST become a double in the future, for
+        //analog arm position rather than an either-or situation
 
         //Elevator
         elevatorAxis = 0;//double //UNTESTED
@@ -147,31 +169,29 @@ public class EverythingOpmodeConfigurable extends LinearOpMode {
         elevatorDownFull = false;//boolean //UNTESTED
         elevatorUpAxis = gamepad2.right_trigger;//double
         elevatorDownAxis = gamepad2.left_trigger;//double
-        elevatorHold = gamepad2.b;//boolean //NOTE: Rarely Used
+        elevatorHold = gamepad2.b;//boolean //NOTE: Rarely Used, may be removed soon
         elevatorMaxPowerUp = gamepad2.dpad_up;//boolean
         elevatorMaxPowerDown = gamepad2.dpad_down;//boolean
-        //More Elevator - stuff that doesn't directly move it
-        elevatorTierUp = gamepad2.right_bumper;//boolean //UNTESTED
-        elevatorTierDown = gamepad2.left_bumper;//boolean //UNTESTED
-        elevatorEncoderReset = gamepad2.back;//boolean //UNTESTED
+
+        //Elevator Miscellany
+        elevatorTierUp = gamepad2.right_bumper;//boolean //BROKEN - 12-15-22
+        elevatorTierDown = gamepad2.left_bumper;//boolean //BROKEN - 12-15-22
+        elevatorEncoderReset = gamepad2.back;//boolean
     }
 
     @Override
     public void runOpMode() {
+        //Hardware Map
         elevatorDrive = hardwareMap.get(DcMotor.class, "Ellyvader");
         elevatorDrive.setDirection(DcMotor.Direction.FORWARD);
         elevatorDrive.setPower(0);
         elevatorDrive.setTargetPosition(0);
 //        elevatorDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
         elevatorDrive.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-        upperLimit = hardwareMap.get(DigitalChannel.class, "UpperLimit");
-        upperLimit.setMode(DigitalChannel.Mode.INPUT);
-        lowerLimit = hardwareMap.get(DigitalChannel.class, "LowerLimit");
-        lowerLimit.setMode(DigitalChannel.Mode.INPUT);
-        servo1 = hardwareMap.get(Servo.class, "servo1");
-        servo2 = hardwareMap.get(Servo.class, "servo2");
-        servo1.setDirection(Servo.Direction.FORWARD);
-        servo2.setDirection(Servo.Direction.FORWARD);
+        clawServo = hardwareMap.get(Servo.class, "servo1");
+        armServo = hardwareMap.get(Servo.class, "servo2");
+        clawServo.setDirection(Servo.Direction.FORWARD);
+        armServo.setDirection(Servo.Direction.FORWARD);
 //        servo1.setPosition(0);
 //        servo2.setPosition(0);
         //wheel motor names
@@ -184,8 +204,11 @@ public class EverythingOpmodeConfigurable extends LinearOpMode {
         leftBack.setDirection(DcMotor.Direction.REVERSE );
         rightBack.setDirection(DcMotor.Direction.FORWARD);
 
-        ServoController scont = servo1.getController();
+        //Make sure the servo PWM is actually enabled - it's not guaranteed.
+        ServoController scont = clawServo.getController();
         scont.pwmEnable();
+
+        //Create all the toggle timers
         ElapsedTime clawDeb = new ElapsedTime(ElapsedTime.Resolution.MILLISECONDS);
         ElapsedTime driveUpDeb = new ElapsedTime(ElapsedTime.Resolution.MILLISECONDS);
         ElapsedTime driveDownDeb = new ElapsedTime(ElapsedTime.Resolution.MILLISECONDS);
@@ -194,44 +217,41 @@ public class EverythingOpmodeConfigurable extends LinearOpMode {
         ElapsedTime elevPowerDownDeb = new ElapsedTime(ElapsedTime.Resolution.MILLISECONDS);
         ElapsedTime elevTierUpDeb = new ElapsedTime(ElapsedTime.Resolution.MILLISECONDS);
         ElapsedTime elevTierDownDeb = new ElapsedTime(ElapsedTime.Resolution.MILLISECONDS);
+
+        //Flags to track claw/arm positions (TODO: These need to die in favor of analog position)
         boolean clawFlag = true;
         boolean armFlag = true;//If arm works funny, try changing this flag.
-        servo1.setPosition(0);
+//        clawServo.setPosition(0);
 
         status = "Ready";
         telemetry.update();
         waitForStart();
 
-//Elevator code
+        //Once play button is pressed...
         status = "Running";
         while (opModeIsActive()) {
 
-            setControls();
+            setControls();//Get the values of all the control inputs.
 
-            if (elevatorAxis != 0) {
-                elevatorPower = elevatorAxis;
+//-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+            //Elevator Code
+            if (elevatorAxis != 0) {//If manipulator is using the elevator axis...
+                elevatorPower = Range.clip(elevatorAxis, elevatorAxisUpperLimit, elevatorAxisLowerLimit);//use it directly.
             } else {
-                double upPower = Range.clip(elevatorUpAxis, 0, elevatorMaxPower);//Makes sure elevator motor never runs above max. power
-                double downPower = Range.clip(elevatorDownAxis, 0, elevatorMaxPower) / 2;//Divide by 2, because elevator is assisted by gravity and fast descent has broken it before!
-
-                //Set elevator motor to power calculated above
-//            if (elevatorUpAxis > 0) {
-//                elevatorDrive.setPower(upPower);
-//            }
-                if (elevatorHold) {//Hold elevator up against gravity (largely unused now)
-                    elevatorDrive.setPower(elevatorHoldPower);
-                } else if (elevatorUpFull) {
+                upPower = Range.clip(elevatorUpAxis, 0, elevatorMaxPower);//Makes sure elevator motor never runs above max. power
+                downPower = Range.clip(elevatorDownAxis, 0, elevatorMaxPower) / 2;//Divide by 2, because elevator is assisted by gravity and fast descent has broken it before!
+                if (elevatorUpFull) {
                     elevatorDrive.setPower(elevatorUpFullPower);
                 } else if (elevatorDownFull) {
                     elevatorDrive.setPower(elevatorDownFullPower);
+                } else if (upPower - downPower == 0) {
+                    elevatorDrive.setTargetPosition(elevatorDrive.getCurrentPosition());
+                    elevatorDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
                 } else {
-                    elevatorPower = upPower - downPower;
+                    elevatorDrive.setPower(upPower - downPower);
                 }
-//            } else if (elevatorDownAxis > 0) {
-//                elevatorDrive.setPower(downPower);
-//            }
             }
-            elevatorDrive.setPower(elevatorPower);
+
             //Adjust Elevator Maximum Power
             if (elevatorMaxPowerUp && elevPowerUpDeb.milliseconds() > maxPowerToggleTimer) {//Change max. power that elevator motor will run at
                 elevatorMaxPower += 0.1;
@@ -240,7 +260,7 @@ public class EverythingOpmodeConfigurable extends LinearOpMode {
                 elevatorMaxPower -= 0.1;
                 elevPowerDownDeb.reset();
 
-                //Tiers
+            //Tiers
             } else if (elevatorTierUp && elevatorTier < 2 && elevTierUpDeb.milliseconds() > elevatorTierToggleTimer) {
                 elevatorDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
                 elevatorDrive.setTargetPosition(elevatorDrive.getCurrentPosition());
@@ -249,13 +269,13 @@ public class EverythingOpmodeConfigurable extends LinearOpMode {
                 elevatorDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
                 elevatorDrive.setTargetPosition(elevatorDrive.getCurrentPosition());
                 elevatorTier -= 1;
-//                if (elevatorTier == 0) {
-//                    elevatorDrive.setTargetPosition(0);//Code below only runs when tier > 0, so it never turns target back to 0 when tier == 0!
-//                }
-                //Set current encoder position to 0
+                if (elevatorTier == 0) {
+                    elevatorDrive.setTargetPosition(0);//Code below only runs when tier > 0, so it never turns target back to 0 when tier == 0!
+                }
+
+            //Set current encoder position to 0
             } else if (elevatorEncoderReset) {
                 elevatorDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-//                sleep(1000);
                 elevatorDrive.setTargetPosition(0);
                 elevatorDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
             } else if (elevatorDrive.getMode() == DcMotor.RunMode.RUN_TO_POSITION && elevatorDrive.getCurrentPosition() == elevatorDrive.getTargetPosition()) {
@@ -267,23 +287,25 @@ public class EverythingOpmodeConfigurable extends LinearOpMode {
             //Actually make the motor go to its designated tier (preliminary code...)
             if (elevatorTier != 0 && !elevatorDrive.isBusy()) {
                 dtemp = elevatorTier * tierMultiplier;
-                int temp = (int) dtemp;
+                temp = (int) dtemp;
                 elevatorDrive.setPower(0.2);
                 status = "Set Tier Power";
                 elevatorDrive.setTargetPosition(temp);
             } if (!elevatorDrive.isBusy()) {
                 elevatorDrive.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
             }
-//            if (temp-15 < elevatorDrive.getCurrentPosition() && elevatorDrive.getCurrentPosition() < temp+15) {
-//                elevatorDrive.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-//            }
+            if (temp-15 < elevatorDrive.getCurrentPosition() && elevatorDrive.getCurrentPosition() < temp+15) {
+                elevatorDrive.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+            }
+
+//----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
             //Claw Code
             if (clawToggle && clawDeb.milliseconds() > manipulatorToggleTimer) {
                 if (clawFlag) {
-                    servo1.setPosition(clawClosedPosition);//Closes claw
+                    clawServo.setPosition(clawClosedPosition);//Closes claw
                 } else {
-                    servo1.setPosition(clawOpenPosition);
+                    clawServo.setPosition(clawOpenPosition);
                 }
                 clawFlag = !clawFlag;
                 clawDeb.reset();
@@ -292,9 +314,9 @@ public class EverythingOpmodeConfigurable extends LinearOpMode {
             //Arm Code
             if (armToggle && armDeb.milliseconds() > manipulatorToggleTimer) {
                 if (armFlag) {
-                    servo2.setPosition(armLeftPosition);
+                    armServo.setPosition(armLeftPosition);
                 } else {
-                    servo2.setPosition(armRightPosition);
+                    armServo.setPosition(armRightPosition);
                 }
                 armDeb.reset();
                 armFlag = !armFlag;
@@ -351,7 +373,7 @@ public class EverythingOpmodeConfigurable extends LinearOpMode {
             if (drivePowerUp && maxDrivePower < 1 && driveUpDeb.milliseconds() > maxPowerToggleTimer) {
                 maxDrivePower += 0.1;
                 driveUpDeb.reset();
-            } else if (drivePowerDown && maxDrivePower > 0.0 && driveDownDeb.milliseconds() < maxPowerToggleTimer) {
+            } else if (drivePowerDown && maxDrivePower > 0 && driveDownDeb.milliseconds() > maxPowerToggleTimer) {
                 maxDrivePower -= 0.1;
                 driveDownDeb.reset();
             }
@@ -383,6 +405,7 @@ public class EverythingOpmodeConfigurable extends LinearOpMode {
 //            telemetry.addData("strafeStick", gamepad1.right_stick_x);
             telemetry.update();
         }
+        telemetry.addData("Status", "Stopped by User");
     }
 }
 
