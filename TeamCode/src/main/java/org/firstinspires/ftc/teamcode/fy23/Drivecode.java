196dc1f6fdb6667f5bee21c1a9fdf09136fd8ae9
WheezieMcriblet <jonahfrosch@gmail.com>
2023-10-14 12:17:41 -0400

    changed the code for mapping buttons to functions

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/fy23/Drivecode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/fy23/Drivecode.java
@@ -37,6 +37,11 @@ public class Drivecode extends LinearOpMode {
         waitForStart();
         runtime.reset();
 
+        // map drive functions to gamepad buttons
+        forwardMovement = rightStickYLinear;
+        strafeMovement = rightStickXLinear;
+        rotateMovement = leftStickXLinear;
+
         while (opModeIsActive()) {
 
             double leftPower;
@@ -47,25 +52,26 @@ public class Drivecode extends LinearOpMode {
             // POV Mode uses left stick to go forward, and right stick to strafe.
             // - This uses basic math to combine motions and is easier to drive straight.
             double drive;
-            double negative;
+            double negative = 0;
             double strafe;
             double turn;
 
-            boolean combinedTranslationControls = true;
-            if (combinedTranslationControls) {
-                drive = -gamepad1.right_stick_y*.75;
-                negative = 0;
-                strafe  =  gamepad1.right_stick_x*.75;
-                turn = gamepad1.left_stick_x*.75;
-            } else {
-                drive = gamepad1.right_trigger;
-                negative = gamepad1.left_trigger;
-                strafe  =  gamepad1.right_stick_x;
-                turn = gamepad1.left_stick_x;
-            }
-
-
-
+            // crappy way of switching control schemes, will delete later
+            // boolean combinedTranslationControls = true;
+            // if (combinedTranslationControls) {
+            //     drive = -gamepad1.right_stick_y*.75;
+            //     negative = 0;
+            //     strafe  =  gamepad1.right_stick_x*.75;
+            //     turn = gamepad1.left_stick_x*.75;
+            // } else {
+            //     drive = gamepad1.right_trigger;
+            //     negative = gamepad1.left_trigger;
+            //     strafe  =  gamepad1.right_stick_x;
+            //     turn = gamepad1.left_stick_x;
+            // }
+            drive = forwardMovement(1,1);
+            strafe  =  strafeMovement(1,1);
+            turn = rotateMovement(1,1);
 
             leftPower    = Range.clip(drive + strafe + turn - negative, -2, 2) ;
             rightPower   = Range.clip(drive - strafe - turn - negative, -2, 2) ;
@@ -81,6 +87,27 @@ public class Drivecode extends LinearOpMode {
         }
 
         telemetry.update();
+    }
+
+    static double forwardMovement() {return 0;}
+    static double strafeMovement() {return 0;}
+    static double rotateMovement() {return 0;}
+
+    static double rightStickYLinear(int gamepadNum, double scaling) {
+        gamepad = (gamepadNum == 2) ? gamepad2 : gamepad1 ;
+        return gamepad.right_stick_y()*scaling;
+    }
+    static double rightStickXLinear(int gamepadNum, double scaling) {
+        gamepad = (gamepadNum == 2) ? gamepad2 : gamepad1 ;
+        return gamepad.right_stick_x()*scaling;
+    }
+    static double triggerLinear(int gamepadNum, double scaling) {
+        gamepad = (gamepadNum == 2) ? gamepad2 : gamepad1 ;
+        return (gamepad.right_trigger()-gamepad.left_trigger())*scaling;
+    }
+    static double leftStickXLinear(int gamepadNum, double scaling) {
+        gamepad = (gamepadNum == 2) ? gamepad2 : gamepad1 ;
+        return gamepad.left_stick_x()*scaling;
     }
 }
 
