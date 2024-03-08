f227d20145de1b42a02a9a498bdd188261c726aa
A League of Their Own <aloto10107@gmail.com>
2024-03-07 20:26:26 -0600

    made claw subsystem

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/fy23/robot/subsystems/Claw.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/fy23/robot/subsystems/Claw.java
@@ -1,8 +1,14 @@
 package org.firstinspires.ftc.teamcode.fy23.robot.subsystems;
 
 import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
 
 public class Claw {
+
+    private Servo servo;
+    private double openPosition;
+    private double closePosition;
+    private State state = State.NONE;
 
     public enum State {
         OPEN,
@@ -12,9 +18,27 @@ public class Claw {
 
     public static class Parameters {
         public boolean present;
+        public double openPosition;
+        public double closePosition;
     }
 
     public Claw(Parameters parameters, HardwareMap hardwareMap) {
+        servo = hardwareMap.get(Servo.class, "clawServo");
+        openPosition = parameters.openPosition;
+        closePosition = parameters.closePosition;
+    }
 
+    public void setState(State state) {
+        this.state = state;
+        if (state == State.OPEN){
+            servo.setPosition(openPosition);
+        }
+        else if (state == State.CLOSED){
+            servo.setPosition(closePosition);
+        }
+    }
+
+    public State getState() {
+        return state;
     }
 }
