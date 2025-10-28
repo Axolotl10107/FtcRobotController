76b377d53072c0f934aa240f732cdb90d3c9eb01
MediumAutomaton <michaelr@wordenhome.net>
2025-10-28 15:52:03 -0500

    DualServo and DualCRServo adapters

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/framework/adapters/DualCRServo.java
@@ -0,0 +1,105 @@
+package org.firstinspires.ftc.teamcode.framework.adapters;
+
+import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.ServoController;
+
+public class DualCRServo implements CRServo {
+
+    private CRServo primary;
+    private CRServo secondary;
+
+    /**
+     * Create a DualServo to represent 2 physical Servos.
+     * @param primary The primary servo. Values will be reported from this servo's perspective.
+     * @param secondary The secondary servo.
+     */
+    public DualCRServo(CRServo primary, CRServo secondary) {
+        this.primary = primary;
+        this.secondary = secondary;
+    }
+
+    @Override
+    /** Returns null! Don't use this! */
+    public ServoController getController() {
+//        return new BlankServoController();
+        // This is to fail fast if someone ends up using this.
+        // It's not safe for something to interact more directly with just one servo.
+        return null;
+    }
+
+    @Override
+    /** Returns -1 */
+    public int getPortNumber() {
+        return -1;
+    }
+
+    @Override
+    /** If the direction to set is different from the primary's current direction, this sets both
+     * servos to the opposite of their current directions. */
+    public void setDirection(CRServo.Direction direction) {
+        if (direction != primary.getDirection()) {
+            if (primary.getDirection() == CRServo.Direction.FORWARD) {
+                primary.setDirection(CRServo.Direction.REVERSE);
+            } else {
+                primary.setDirection(CRServo.Direction.FORWARD);
+            }
+            if (secondary.getDirection() == CRServo.Direction.FORWARD) {
+                secondary.setDirection(CRServo.Direction.REVERSE);
+            } else {
+                secondary.setDirection(CRServo.Direction.FORWARD);
+            }
+        }
+    }
+
+    @Override
+    /** Returns the direction of the primary servo. */
+    public CRServo.Direction getDirection() {
+        return primary.getDirection();
+    }
+
+    @Override
+    public void setPower(double power) {
+        primary.setPower(power);
+        secondary.setPower(power);
+    }
+
+    @Override
+    /** Returns the average power between both servos. */
+    public double getPower() {
+        double primaryPower = primary.getPower();
+        double secondaryPower = secondary.getPower();
+        return ( primaryPower + secondaryPower ) / 2;
+    }
+
+    @Override
+    public Manufacturer getManufacturer() {
+        return primary.getManufacturer();
+    }
+
+    @Override
+    public String getDeviceName() {
+        return "Dual " + primary.getDeviceName() + " " + secondary.getDeviceName();
+    }
+
+    @Override
+    public String getConnectionInfo() {
+        return primary.getConnectionInfo();
+    }
+
+    @Override
+    public int getVersion() {
+        return primary.getVersion();
+    }
+
+    @Override
+    public void resetDeviceConfigurationForOpMode() {
+        primary.resetDeviceConfigurationForOpMode();
+        secondary.resetDeviceConfigurationForOpMode();
+    }
+
+    @Override
+    public void close() {
+        primary.close();
+        secondary.close();
+    }
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/framework/adapters/DualServo.java
@@ -0,0 +1,111 @@
+package org.firstinspires.ftc.teamcode.framework.adapters;
+
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.hardware.ServoController;
+
+public class DualServo implements Servo {
+
+    private Servo primary;
+    private Servo secondary;
+
+    /**
+     * Create a DualServo to represent 2 physical Servos.
+     * @param primary The primary servo. Values will be reported from this servo's perspective.
+     * @param secondary The secondary servo.
+     */
+    public DualServo(Servo primary, Servo secondary) {
+        this.primary = primary;
+        this.secondary = secondary;
+    }
+
+    @Override
+    /** Returns null! Don't use this! */
+    public ServoController getController() {
+//        return new BlankServoController();
+        // This is to fail fast if someone ends up using this.
+        // It's not safe for something to interact more directly with just one servo.
+        return null;
+    }
+
+    @Override
+    /** Returns -1 */
+    public int getPortNumber() {
+        return -1;
+    }
+
+    @Override
+    /** If the direction to set is different from the primary's current direction, this sets both
+     * servos to the opposite of their current directions. */
+    public void setDirection(Direction direction) {
+        if (direction != primary.getDirection()) {
+            if (primary.getDirection() == Direction.FORWARD) {
+                primary.setDirection(Direction.REVERSE);
+            } else {
+                primary.setDirection(Direction.FORWARD);
+            }
+            if (secondary.getDirection() == Direction.FORWARD) {
+                secondary.setDirection(Direction.REVERSE);
+            } else {
+                secondary.setDirection(Direction.FORWARD);
+            }
+        }
+    }
+
+    @Override
+    /** Returns the direction of the primary servo. */
+    public Direction getDirection() {
+        return primary.getDirection();
+    }
+
+    @Override
+    public void setPosition(double position) {
+        primary.setPosition(position);
+        secondary.setPosition(position);
+    }
+
+    @Override
+    /** Returns the average position between both servos. */
+    public double getPosition() {
+        double primaryPos = primary.getPosition();
+        double secondaryPos = secondary.getPosition();
+        return ( primaryPos + secondaryPos ) / 2;
+    }
+
+    @Override
+    public void scaleRange(double min, double max) {
+        primary.scaleRange(min, max);
+        secondary.scaleRange(min, max);
+    }
+
+    @Override
+    public Manufacturer getManufacturer() {
+        return primary.getManufacturer();
+    }
+
+    @Override
+    public String getDeviceName() {
+        return "Dual " + primary.getDeviceName() + " " + secondary.getDeviceName();
+    }
+
+    @Override
+    public String getConnectionInfo() {
+        return primary.getConnectionInfo();
+    }
+
+    @Override
+    public int getVersion() {
+        return primary.getVersion();
+    }
+
+    @Override
+    public void resetDeviceConfigurationForOpMode() {
+        primary.resetDeviceConfigurationForOpMode();
+        secondary.resetDeviceConfigurationForOpMode();
+    }
+
+    @Override
+    public void close() {
+        primary.close();
+        secondary.close();
+    }
+}
