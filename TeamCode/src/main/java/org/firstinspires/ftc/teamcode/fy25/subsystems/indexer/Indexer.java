8c71393bc077fd36af4b61fc21a2ee9598c28e2e
eb-10 <beckevan64@gmail.com>
2026-01-16 18:07:07 -0600

    Indexer subsystem

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/fy25/subsystems/indexer/Indexer.java
@@ -0,0 +1,40 @@
+package org.firstinspires.ftc.teamcode.fy25.subsystems.indexer;
+
+import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+
+public interface Indexer {
+
+    enum Index {
+        A,
+        B,
+        C
+    }
+
+    class Parameters {
+        /** Create a Parameters object and provide necessary parameters.
+         * @param present Is this subsystem installed on this robot?
+         */
+        public Parameters(boolean present) {
+            this.present = present;
+        }
+
+        /** You have already set this in the constructor and cannot set it again. */
+        public final boolean present;
+
+        public DcMotorEx encoderMotor;
+        public CRServo indexerServo;
+        public double ticksPerRevolution; // 8192
+    }
+
+    void goTo(Index index);
+
+    void next();
+
+    void prepIntake(Index index);
+
+    void intake();
+
+    void update();
+
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/fy25/subsystems/indexer/IndexerBlank.java
@@ -0,0 +1,29 @@
+package org.firstinspires.ftc.teamcode.fy25.subsystems.indexer;
+
+public class IndexerBlank implements Indexer {
+
+    @Override
+    public void goTo(Index index) {
+
+    }
+
+    @Override
+    public void next() {
+
+    }
+
+    @Override
+    public void prepIntake(Index index) {
+
+    }
+
+    @Override
+    public void intake() {
+
+    }
+
+    @Override
+    public void update() {
+
+    }
+}
